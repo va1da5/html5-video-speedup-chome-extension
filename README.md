778be1cdcc2574cf2260f7b9040d286d80ea92a8
Vaidas <vaidas.vaiciunas@inbox.lt>
2018-10-06 17:09:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# HTML5 Video Speedup Chrome Extension
+
+Minimal Chrome extension to increase/decrease video playback speed. No permissions required.
+
+Installation:
+ * Enable developer mode in Chrome extensions menu
+ * Click `Load unpacked` extension and open extension directory
+
---
+++ scripts/content.js
@@ -0,0 +1,49 @@
+function findPlayingVideo(){
+    let videos = document.querySelectorAll("video");
+    let playing = Array.from(videos).filter(v => !v.paused);
+    if (playing.length > 0){
+        return playing;
+    }
+    return []
+}
+
+function showSpeed(video){
+    var el = document.createElement("div");
+    el.innerHTML = `Speed: ${video.playbackRate.toString().slice(0, 4)}x`;
+    el.style.position = "absolute";
+    el.style.top = "20px";
+    el.style.right = "20px";
+    el.style.background = "#dd0";
+    el.style.borderRadius = "3px";
+    el.style.padding = "10px";
+    el.style.color = "#d00";
+    el.style.zIndex = 10000;
+    el.style.fontSize = "15px";
+    video.parentElement.appendChild(el);
+
+    setTimeout(() => {
+        el.parentNode.removeChild(el);
+        el = null;
+    }, 500);
+}
+
+function changeVideoPlaybackRate(videos, value){
+    let videos = findPlayingVideo();
+    videos.forEach(video => {
+        video.playbackRate = video.playbackRate + value;
+        showSpeed(video);
+    });
+    return videos;
+}
+
+function shortkeys(event){
+    if (event.key === "["){
+        changeVideoPlaybackRate(videos, -0.10);
+    }
+    if (event.key === "]") {
+        changeVideoPlaybackRate(videos, 0.10);
+
+    }
+}
+
+window.addEventListener("keyup", shortkeys);
